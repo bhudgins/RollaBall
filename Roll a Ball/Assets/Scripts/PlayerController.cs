4ad9c75263b62119149a03360e37accb83210c86
bhudgins <bhudgins@HARDING.EDU>
2018-01-24 15:42:35 -0600

    Added project files

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	public Text timerText;
+	private Rigidbody rb;
+
+	private int count;
+	private float startTime;
+	private bool stopTimer = false;
+	private bool gameOver = false;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+		startTime = Time.time;
+	}
+
+	void FixedUpdate()
+	{
+		
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void Update()
+	{
+		if (stopTimer) {
+			return;
+		}
+		float t = Time.time - startTime;
+
+		string minutes = ((int)t / 60).ToString ();
+		string seconds = (t % 60).ToString ("f0");
+		timerText.text = minutes + ":" + seconds;
+
+		if (minutes == "1") {
+			winText.text = "You Lost!";
+			gameOver = true;
+			stopTimer = true;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (gameOver) {
+		} else {
+			if (other.gameObject.CompareTag ("Pickup")) {
+				other.gameObject.SetActive (false);
+				count = count + 1;
+				SetCountText ();
+			}
+			if (other.gameObject.CompareTag ("UniquePickup")) {
+				other.gameObject.SetActive (false);
+				count = count + 5;
+				SetCountText ();
+			}	
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 16) {
+			winText.text = "You Win!";
+			stopTimer = true;
+
+		}
+
+	}
+}
+
